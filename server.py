982b94ac8cafeaa604f11f6af7bc6c5a5d00b7a3
Zack Grimley <grimzack@gmail.com>
2017-07-07 17:47:57 -0500

    Functioning server class, supporting multiple client connections.
    Rough working example where the server supports multiple clients
    connecting in unison.
    
    Currently using basic threading knowledge to create a new thread for
    each connected client and just echoing any text sent by client.

--- server.py
+++ server.py
@@ -14,7 +14,9 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 1111              # Arbitrary non-privileged port
 
 
-def server_receive_message(conn):
+def server_receive_message(conn, addr):
+	# conn = args[0]
+	# addr = args[1]
 	print 'Connected by', addr
 	while 1:
 	    data = conn.recv(1024)
@@ -27,19 +29,18 @@ client_cxns = []
 client_threads = []
 
 def accept_clients():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((HOST, PORT))
 	while 1: 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind((HOST, PORT))
 		s.listen(1)
 		conn, addr = s.accept()
 		client_cxns.append(conn)
-		thread = threading.Thread(target=server_receive_message, args=(conn))
+		thread = threading.Thread(target=server_receive_message, args=(conn,addr))
 		client_threads.append(thread)
 		thread.start()
 
 def main():
+	print "Running main()"
 	accept_clients()
 
-
-
-
+main()
