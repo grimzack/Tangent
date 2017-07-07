8d425f4f929660ed838367f9764daea00f37ea4a
Zack Grimley <grimzack@gmail.com>
2017-07-07 18:06:12 -0500

    Added arguments to client program for user to specify their name upon launching.
    Above says it all. Also includes minor restructuring of code to put it
    in (poorly organized) methods.

--- client.py
+++ client.py
@@ -11,16 +11,34 @@ import socket
 import sys
 
 HOST = 'localhost'    # The remote host
-PORT = 1111              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+PORT = 1111           # The same port as used by the server
+username = ''
 
-while 1:
-	message = raw_input("user$ ")
-	if (message == '/exit'):
-		break
+def send_message():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
 
-	s.sendall(message)
-	data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+	print "Username = " + username
+	while 1:
+		message = raw_input(username + "$ ")
+		if (message == '/exit'):
+			message = 'Leaving chat'
+			break
+
+		s.sendall(message)
+		data = s.recv(1024)
+	s.close()
+
+def main():
+	global username
+	for arg in sys.argv:
+		if 'user' in arg:
+			username = arg[5:]
+
+	if (username == ''):
+		username = "John"
+
+	print "Connecting with user " + username
+	send_message()
+
+main()
